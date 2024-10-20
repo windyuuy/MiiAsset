ce19e4105b3120b77386143a1d37b66beae83d30
windy <kugoo11v12@126.com>
2024-10-21 02:04:09 +0800

    U: 支持清理微信小游戏缓存

---
+++ Assets/Framework/MiiAsset/AddressablesExt/AddressablesExt.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MiiAsset.AddressablesExt
+{
+	public static class AddressablesExt
+	{
+		public static Task<bool> CleanUpAddressablesCache()
+		{
+#if UNITY_WEBGL && !UNITY_EDITOR
+			var ts = new TaskCompletionSource<bool>();
+			var wxfs = WeChatWASM.WX.GetFileSystemManager();
+			var dir = $"{WeChatWASM.WX.env.USER_DATA_PATH}/__GAME_FILE_CACHE/hotres/";
+			if (wxfs.AccessSync(dir) == "access:ok")
+			{
+				wxfs.Rmdir(new WeChatWASM.RmdirParam
+				{
+					success = (resp) => { ts.SetResult(true); },
+					fail = (resp) =>
+					{
+						Debug.LogError($"remove aa-cache failed, errCode: {resp.errCode}, errMsg: {resp.errMsg}");
+						ts.SetResult(false);
+					},
+					dirPath = dir,
+					recursive = true,
+				});
+
+				try
+				{
+					Caching.ClearCache();
+				}
+				catch (Exception exception)
+				{
+					Debug.LogException(exception);
+				}
+			}
+			else
+			{
+				Debug.Log("本地存储为空");
+			}
+
+			return ts.Task;
+#else
+			try
+			{
+				Caching.ClearCache();
+				return Task.FromResult(true);
+			}
+			catch (Exception exception)
+			{
+				Debug.LogException(exception);
+				return Task.FromResult(false);
+			}
+#endif
+		}
+	}
+}
