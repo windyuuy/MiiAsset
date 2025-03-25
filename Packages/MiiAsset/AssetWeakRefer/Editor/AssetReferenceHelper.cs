82a3591cfabd44006db7efd8565983aea1d92b62
windy <kugoo11v12@126.com>
2025-03-25 11:43:56 +0800

    U: 完善资源间接引用功能

---
+++ Packages/MiiAsset/AssetWeakRefer/Editor/AssetReferenceHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MiiAsset.AssetWeakRefer.Runtime;
+using UnityEditor;
+
+namespace MiiAsset.AssetWeakRefer.Editor
+{
+	public static class AssetReferenceHelper
+	{
+		public static T LoadFromAsset<T>(UnityEngine.Object asset1) where T : AssetReference
+		{
+			var path = AssetDatabase.GetAssetPath(asset1);
+			var guid1 = AssetDatabase.AssetPathToGUID(path);
+			var assetReference = (T)Activator.CreateInstance(typeof(T), guid1);
+			return assetReference;
+		}
+
+		public static T LoadFromAssetPath<T>(string path) where T : AssetReference
+		{
+			var guid1 = AssetDatabase.AssetPathToGUID(path);
+			var assetReference = (T)Activator.CreateInstance(typeof(T), guid1);
+			return assetReference;
+		}
+	}
+}
