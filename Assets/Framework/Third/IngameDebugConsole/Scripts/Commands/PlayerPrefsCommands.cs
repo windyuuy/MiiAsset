eb3eb2c3f6819e8033f22f59eebf636345827cb3
ljb <kugoo11v12@126.com>
2025-09-09 10:39:58 +0800

    U: 提交console, 完善测试用例

---
+++ Assets/Framework/Third/IngameDebugConsole/Scripts/Commands/PlayerPrefsCommands.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace IngameDebugConsole.Commands
+{
+	public class PlayerPrefsCommands
+	{
+		[ConsoleMethod( "prefs.int", "Returns the value of an Integer PlayerPrefs field" ), UnityEngine.Scripting.Preserve]
+		public static string PlayerPrefsGetInt( string key )
+		{
+			if( !UnityEngine.PlayerPrefs.HasKey( key ) ) return "Key Not Found";
+			return UnityEngine.PlayerPrefs.GetInt( key ).ToString();
+		}
+
+		[ConsoleMethod( "prefs.int", "Sets the value of an Integer PlayerPrefs field" ), UnityEngine.Scripting.Preserve]
+		public static void PlayerPrefsSetInt( string key, int value )
+		{
+			UnityEngine.PlayerPrefs.SetInt( key, value );
+		}
+
+		[ConsoleMethod( "prefs.float", "Returns the value of a Float PlayerPrefs field" ), UnityEngine.Scripting.Preserve]
+		public static string PlayerPrefsGetFloat( string key )
+		{
+			if( !UnityEngine.PlayerPrefs.HasKey( key ) ) return "Key Not Found";
+			return UnityEngine.PlayerPrefs.GetFloat( key ).ToString();
+		}
+
+		[ConsoleMethod( "prefs.float", "Sets the value of a Float PlayerPrefs field" ), UnityEngine.Scripting.Preserve]
+		public static void PlayerPrefsSetFloat( string key, float value )
+		{
+			UnityEngine.PlayerPrefs.SetFloat( key, value );
+		}
+
+		[ConsoleMethod( "prefs.string", "Returns the value of a String PlayerPrefs field" ), UnityEngine.Scripting.Preserve]
+		public static string PlayerPrefsGetString( string key )
+		{
+			if( !UnityEngine.PlayerPrefs.HasKey( key ) ) return "Key Not Found";
+			return UnityEngine.PlayerPrefs.GetString( key );
+		}
+
+		[ConsoleMethod( "prefs.string", "Sets the value of a String PlayerPrefs field" ), UnityEngine.Scripting.Preserve]
+		public static void PlayerPrefsSetString( string key, string value )
+		{
+			UnityEngine.PlayerPrefs.SetString( key, value );
+		}
+
+		[ConsoleMethod( "prefs.delete", "Deletes a PlayerPrefs field" ), UnityEngine.Scripting.Preserve]
+		public static void PlayerPrefsDelete( string key )
+		{
+			UnityEngine.PlayerPrefs.DeleteKey( key );
+		}
+
+		[ConsoleMethod( "prefs.clear", "Deletes all PlayerPrefs fields" ), UnityEngine.Scripting.Preserve]
+		public static void PlayerPrefsClear()
+		{
+			UnityEngine.PlayerPrefs.DeleteAll();
+		}
+	}
+}
--- Assets/Scenes/JKLJWF.cs
+++ Assets/Scenes/JKLJWF.cs
@@ -7,14 +7,14 @@ public class JKLJWF : MonoBehaviour
 	public Canvas canvas;
 
 	// Start is called before the first frame update
-	async void Start()
+	public async void Display()
 	{
 		try
 		{
 			await AssetLoader.Init();
 			Debug.Log("AssetLoader.Init");
 			// await AssetLoader.LoadLocalCatalog();
-			await AssetLoader.UpdateCatalog("http://127.0.0.1:8081/bundles/");
+			await AssetLoader.UpdateCatalog("http://192.168.137.3:8081/");
 			Debug.Log("AssetLoader.LoadLocalCatalog");
 			var obj = await AssetLoader.InstantiateAsync("Assets/Bundles/BB/TestLoad.prefab", canvas.transform).Task;
 			Debug.Log("AssetLoader.InstantiateAsync");
