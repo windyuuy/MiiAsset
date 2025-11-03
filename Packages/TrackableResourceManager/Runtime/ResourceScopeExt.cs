cb44fe2188a7105b4b6d4e00637dcf249d4d265f
windy <kugoo11v12@126.com>
2025-11-03 10:53:21 +0800

    U: scope支持instantiate

--- Packages/TrackableResourceManager/Runtime/ResourceScopeExt.cs
+++ Packages/TrackableResourceManager/Runtime/ResourceScopeExt.cs
@@ -33,5 +33,34 @@ namespace TrackableResourceManager.Runtime
 		{
 			return wrap.Token.UnLoadAsset<T>(resKey);
 		}
+
+		public static Task<GameObject> Instantiate(this IResourceScope wrap, string key)
+		{
+			return wrap.Instantiate(key, null);
+		}
+
+		public static async Task<GameObject> Instantiate(this IResourceScope wrap, string key, Transform parent)
+		{
+			var asset = await wrap.LoadAsset<GameObject>(key);
+			var obj = GameObject.Instantiate(asset, parent);
+			return obj;
+		}
+
+		public static async Task<bool> ReleaseInstance(this IResourceScope wrap, string key, GameObject gameObject)
+		{
+		#if UNITY_EDITOR
+			if (!Application.isPlaying)
+			{
+				GameObject.DestroyImmediate(gameObject);
+			}
+			else
+		#endif
+			{
+				GameObject.Destroy(gameObject);
+			}
+
+			await wrap.UnLoadAsset<GameObject>(key);
+			return true;
+		}
 	}
 }
